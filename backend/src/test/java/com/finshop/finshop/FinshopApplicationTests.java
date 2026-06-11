6727754abcfcad9ea5694d27ab57b040200ec34b
Teame Correa <teamecorrea13@gmail.com>
2026-06-11 00:43:16 +0000

    feat: initialisation de la stucture spring boot

---
+++ backend/src/test/java/com/finshop/finshop/FinshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finshop.finshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
